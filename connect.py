58166b0ad8d1db41fb3941ba2d70f4bf3f2fe913
ZachGr <107058056+ZachGr@users.noreply.github.com>
2022-06-21 23:17:27 -0400

    Added additional features
    Changed wording and added csv read and push to sql on connect.py

--- connect.py
+++ connect.py
@@ -2,8 +2,10 @@ import mysql.connector
 import password
 from datetime import datetime
 import requests
+import pandas as pd
+from sqlalchemy import create_engine
 
-
+#creating a class to connect to the database
 class Database:
     my_db = my_cursor = None
 
@@ -16,14 +18,10 @@ class Database:
             database="btc"
         )
         my_cursor = my_db.cursor()
-        #my_cursor.commit()
-
-    """ def __del__(self):
-        my_db.commit() """
 
 
 class account(Database):
-
+    #runs select command in sequal with the user inputs
     def select(self, id):
         sql = "SELECT * FROM account ORDER BY id {}".format(id)
 
@@ -34,11 +32,10 @@ class account(Database):
         except Exception as e:
             return e
 
-        #for r in result:
         return result
 
+    #Allows user to create an account with the values they inputed
     def insert(self, data):
-
         sql = "INSERT INTO account (fname, lname,btcAmount,tdate) VALUES ('{}','{}', {}, '{}')".format(data[0], data[1], data[2], data[3])
         try:
             my_cursor.execute(sql)
@@ -46,11 +43,10 @@ class account(Database):
         except Exception as e:
             return e
 
-        #return my_cursor.lastrowid
-
+    #Allows ADMIN to be able to delete from sql
     def delete(self, table, idName, id):
         sql = "DELETE FROM {} WHERE {} = {}".format(table, idName, id)
-        print(sql)
+        #print(sql)
 
         try:
             my_cursor.execute(sql)
@@ -58,6 +54,7 @@ class account(Database):
         except Exception as e:
             return e
 
+#Allows user to update the amount of Bitcoin they have
     def update(self, amount, index):
 
         sql = "UPDATE account SET btcAmount = {} WHERE ID = {}".format(amount, index)
@@ -67,6 +64,7 @@ class account(Database):
         try:
             my_cursor.execute(sql)
             my_db.commit()
+            print("Account {} has been updated to have {} bitcoin".format(index, amount))
 
         except Exception as e:
             return e
@@ -76,10 +74,12 @@ class account(Database):
 db = Database()
 ac = account()
 
+#Allows user to choose how they want to continue
 def user(cursor, connection):
     choice = ""
-    print("Type: \n1 for creating a new account\n2 to access your account\n3 to see the value of your Bitcoin\n4 ADMIN ONLY")
+    print("Type: \n1 Create a new account\n2 Edit the amount of Bitcoin in your account\n3 See the value of your Bitcoin\n4 See all accounts and Remove accounts (ADMIN ONLY)\n5 To exit")
     choice = input("How would you like to proceed: ")
+    #Checks to make sure that the input is an int and a correct option
     it_is = checkInt(choice)
     while(it_is != True):
         choice = input("How would you like to proceed: ")
@@ -92,29 +92,34 @@ def user(cursor, connection):
         value(cursor, connection)
     elif(choice == '4'):
         admin(cursor, connection)
+    elif(choice == '5'):
+        again = 'n'
+        pass
     else:
         print("Not an Option")
+        user(cursor, connection)
     #connection.close()
 
-#THIS IS INSERTING
+#This asks for user input and then calls the insert class to run the sql code
 def inserting():
     fname = input("Enter first name: ")
+    #Checking that user input is correct and usable
     while(fname == ''):
         fname = input("Enter first name: ")
     lname = input("Enter last name: ")
     while(lname == ''):
         lname = input("Enter last name: ")
-    btcAmount = input("Enter the amount of bitcoins do you own: ")
-    #btcAmount = int(btcAmount)
+    btcAmount = input("Enter the amount of Bitcoin you own: ")
     it_is = checkInt(btcAmount)
     while(it_is != True):
-        btcAmount = input("Enter the amount of bitcoins do you own: ")
+        btcAmount = input("Enter the amount of Bitcoin you own: ")
         it_is = checkInt(btcAmount)
     tdate =  datetime.now().date()
+    #Example of a collection
     data = [fname, lname, btcAmount, tdate]
     ac.insert(data)
 
-    #Password
+    #Allows user to input a password for their account and checks to make sure one has been added
     pword = input("create a password for this account: ")
     while(pword == ''):
         pword = input("create a password for this account: ")
@@ -125,11 +130,11 @@ def inserting():
     userIndex = my_cursor.execute("SELECT MAX(id) FROM account;")
     userIndex = my_cursor.fetchall()
     userIndex = cleanTxt(userIndex)
-    print("The index for your account is {}, make sure you remember this so you can sign in later.".format(userIndex))
+    print("Your account ID is {}, make sure you remember this so you can sign in later.".format(userIndex))
     #my_db.commit()
 
 
-#THIS IS UPDATING
+#This asks for user input and then calls the update class to run the sql code
 def updating():
     index = input("Type in your account ID: ")
     it_is = checkInt(index)
@@ -149,9 +154,9 @@ def updating():
             while(it_is != True):
                 amount = input("How much bitcoin do you now have? ")
                 it_is = checkInt(amount)
-            print("yo")
             ac.update(amount, index)
 
+#a function that checks the input for passwords when called
 def checkPsw(cursor, index):
     pswcheck = False
     pwordAttempt = input("What is the password for this account? ")
@@ -168,6 +173,7 @@ def checkPsw(cursor, index):
     
     return pswcheck
 
+#A function that removes unwanted characters from strings to be used later in the code
 def cleanTxt(tuple):
     string = str(tuple)
     string = string.replace('"','')
@@ -179,7 +185,7 @@ def cleanTxt(tuple):
     string = string.replace("[",'')
     return string
 
-
+#a function to check that the ID enter is an int and exists within a table
 def checkID(cursor, index):
     checkID = False
     
@@ -192,11 +198,12 @@ def checkID(cursor, index):
     checkID = True
     #print(result)
     if(result == ''):
-        print("index does not exist")
+        print("Account ID entered is not an option")
         checkID = False
     
     return checkID
 
+#Uses an api to get the current price of bitcoin and calculates the users value based off of that
 def value(cursor, connection):
     index = input("What is your account ID? ")
     it_is = checkInt(index)
@@ -219,10 +226,10 @@ def value(cursor, connection):
             result = float(result)
             currentValue = trackBitcoin() * result
             currentValueUSD = '${:,.2f}'.format(currentValue)
-            print("You currently have {} worth of Bitcoin".format(currentValueUSD)) 
-            #Comparing value from Date##############
+            #Compares price of bitcoin from a specific date and compares it to users amount
+            print("The amount of bitcoin you have, {}, is worth {}".format(result, currentValueUSD)) 
+            #Comparing value from Date#
             date = input("Enter a date to compare the price to between(04-19-2021 and 04-19-2022):\n")
-            #print(checkDate(cursor, connection, date))
             date = str(date)
             thenPrice = priceDate(cursor, connection, date)
             if(thenPrice == False):
@@ -233,14 +240,16 @@ def value(cursor, connection):
                 if(thenPrice > currentValue):
                     upDown = thenPrice - currentValue
                     upDown = '${:,.2f}'.format(upDown)
-                    print("You are down {} from {}".format(upDown, date))
+                    thenPrice = '${:,.2f}'.format(thenPrice)
+                    print("You are down {} from the {} price of {}".format(upDown, date, thenPrice))
                 elif(thenPrice < currentValue):
                     upDown = currentValue - thenPrice
                     upDown = '${:,.2f}'.format(upDown)
-                    print("You are up {} from {}".format(upDown, date))
+                    print("You are up {} from the {} price of {}".format(upDown, date, thenPrice))
                 else:
                     print("You have neither gained or lost money")
 
+#An API that gets the current price of Bitcoin
 def trackBitcoin():
     url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
     response = requests.get(url).json()
@@ -249,18 +258,14 @@ def trackBitcoin():
     date = datetime.now().date()
     return price
 
+#Gets the price for the user inputed date
 def priceDate(cursor, connection, date):
-    #sql = "SELECT open FROM btcprice WHERE;"
     sql = "SELECT open FROM btcPrice WHERE date = '{}';".format(date)
-    # WHERE date = {};".format(date)
     connection.commit()
     cursor.execute(sql)
     result = cursor.fetchall()
-    #print(result)
-    #result = result[0]
     priceAtDate = cleanTxt(result)
     checkDate = True
-    #print(result)
     if(priceAtDate == ''):
         print("Date is not in range or was typed incorrectly")
         checkDate = False
@@ -270,7 +275,6 @@ def priceDate(cursor, connection, date):
         return (priceAtDate)
 
 def admin(cursor, connection):
-    #ac = account()
     index = 1
     pswCheck=checkPsw(cursor, index)
     if(pswCheck == False):
@@ -279,17 +283,17 @@ def admin(cursor, connection):
         result = ac.select("ASC")
         for r in result:
             print(r)
-        rid = input("Type the index of which account you want to delete: ")
+        rid = input("Type the account ID of the account you would like to delete: ")
         it_is = checkInt(rid)
         while(it_is != True):
-            rid = input("Type the index of which account you want to delete: ")
+            rid = input("Type the account ID of the account you would like to delete: ")
             it_is = checkInt(rid)
         while(rid == "1"):
             print("Can not delete Admin")
-            rid = input("Type the index of which account you want to delete: ")
+            rid = input("Type the account ID of the account you would like to delete: ")
             it_is = checkInt(rid)
             while(it_is != True):
-                rid = input("Type the index of which account you want to delete: ")
+                rid = input("Type the account ID of the account you would like to delete: ")
                 it_is = checkInt(rid)
         IDcheck = checkID(cursor, rid)
         if(IDcheck == False):
@@ -297,12 +301,6 @@ def admin(cursor, connection):
         else:
             ac.delete("psw", "accountid", rid)
             ac.delete("account", "ID", rid)
-            #sql = "DELETE FROM psw WHERE accountID = {};".format(rid)
-            #cursor.execute(sql)
-            #connection.commit()
-            #sql = "DELETE FROM account WHERE ID = {};".format(rid)
-            #cursor.execute(sql)
-            #connection.commit()
 
     
             sql = "SELECT * FROM account;"
@@ -318,7 +316,14 @@ def checkInt(x):
     except ValueError:
         it_is = False
     return it_is
-#inserting()
-#updating()
-#value(my_cursor, my_db)
-#user()
+
+#only run if need to fill btcPrice database with data from csv file
+def btcdatabase():
+    with open("BTC-USD.csv", 'r') as file:
+        content = file.readlines()
+    header = content[:1]
+    rows = content[1:]
+    data= pd.read_csv("BTC-USD.csv")
+    df = data
+    engine = create_engine("mysql+pymysql://root:"+password.myPassword+"@localhost/btc")
+    df.to_sql(name = 'btcPrice', con=engine, if_exists='append', index=False)
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #import sqlTesting as sqlT
+from matplotlib.pyplot import connect
 import connect as sqlT
+import os
 
+
+#Runs through connect.py until the user is done
 again = 'y'
 while(again == 'y'):
+    os.system('CLS')
     sqlT.user(sqlT.my_cursor, sqlT.my_db)
     again = input("Press y to continue and n to quit: ")
     while(again != 'y' and again != 'n'):
